43344861649bcf865073a41d1b6bde5e8854bb98
Jared Black <jaredblackjcb@gmail.com>
2022-10-09 00:19:44 -0500

    Create annuity calculator

--- fintools/calculator/forms.py
+++ fintools/calculator/forms.py
@@ -1,1 +1,5 @@
 from django import forms
+class AnnuityCalculator(forms.Form):
+    payment = forms.DecimalField(label="Payment", max_digits=10, decimal_places=2, help_text="Payment is the amount of money received from an annuity in a given period")
+    interest_rate = forms.DecimalField(label="Interest Rate", max_digits=5, decimal_places=2, help_text="Interest Rate per period")
+    periods = forms.IntegerField(label="Periods", help_text='Number of periods')
--- fintools/calculator/views.py
+++ fintools/calculator/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django import views
+from django.views import View
+from . import forms
 
 # Create your views here.
 def pmi_calculator(request):
     context_dict = {}
     return render(request, 'calculator/pmi_calculator.html', context=context_dict)
+
+def annuity_calculator(request):
+    annuity_calculator_form = forms.AnnuityCalculator(request.POST)
+    future_value = 0
+    context_dict = {'future_value': future_value, 'annuity_calculator_form': annuity_calculator_form}
+    if request.method == 'POST':
+        if annuity_calculator_form.is_valid():
+            payment = annuity_calculator_form.cleaned_data['payment']
+            interest_rate = annuity_calculator_form.cleaned_data['interest_rate']
+            periods = annuity_calculator_form.cleaned_data['periods']
+            future_value = (payment * ((1 + interest_rate) ** periods - 1) / interest_rate)
+            context_dict['future_value'] = future_value
+            print(payment)
+            print(interest_rate)
+            print(periods)
+            print(future_value)
+            print(context_dict)
+    return render(request, 'calculator/annuity_calculator.html', context=context_dict)
+
