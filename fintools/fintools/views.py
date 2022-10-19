7ddf650f01c5b8808c1c406c90963a2ff1bf1a0f
Jared Black <jaredblackjcb@gmail.com>
2022-10-19 00:26:36 -0500

    Add error handling logic for mailing list form

--- fintools/fintools/views.py
+++ fintools/fintools/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from calculator import forms
+from calculator.models import MailingMember
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    form = forms.MailingList(request.POST)
+    form = forms.MailingList()
+    context = {'form': form}
     if request.method == 'POST':
+        form = forms.MailingList(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
-            print(name, email)
-    return render(request, 'index.html', context={'form': form})
+            member, created = MailingMember.objects.get_or_create(name=name, email=email)
+            if created:
+                member.save()
+                messages.success(request, "Thanks for signing up! We'll be in touch soon!")
+            else:
+                messages.error(request, "User already exists. Please enter a unique name and email address.")
+            return redirect('index')
+        else:
+            messages.error(request, form.errors['email'])
+    return render(request, 'index.html', context=context)
