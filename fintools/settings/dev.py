80efc49cd0271fe9bd3364006bc06b250d61944a
Jared Black <jaredblackjcb@gmail.com>
2022-10-26 23:03:53 -0500

    Major refactor to move everything up to the top level

--- fintools/settings/dev.py
+++ fintools/settings/dev.py
@@ -9,4 +9,4 @@ SECRET_KEY = 'django-insecure-(47**&$39lh&(r=hn$d(1l8=md%%kxi@bon$--v+3!lfjq$wl(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
