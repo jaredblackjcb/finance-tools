ea80606c275d90c23afd7878714692c99de8ddeb
Jared Black <jaredblackjcb@gmail.com>
2022-11-05 11:38:09 -0500

    Set up webpack

---
+++ assets/scripts/charts.js
@@ -0,0 +1,22 @@
+import * as echarts from 'echarts';
+
+// initialize the echarts instance
+var myChart = echarts.init(document.getElementById('chart-container'),);
+// Draw the chart
+myChart.setOption({
+  title: {
+    text: 'ECharts Getting Started Example'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'sales',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+});
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,8 @@
+import './navbar'
+import './pmi_calculator'
+import './charts'
+import 'echarts'
+
 const msgCloseButton = document.querySelector('button.delete');
 const successMessage = document.querySelector('article.message');
 
