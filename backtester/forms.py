1ff4347e62081ea700e207527f59d91f8857c772
Jared Black <jaredblackjcb@gmail.com>
2022-11-15 18:47:49 -0600

    Fix mutual fund bug

--- backtester/forms.py
+++ backtester/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class BactesterForm(forms.Form):
+class BacktesterForm(forms.Form):
     ticker = forms.CharField(max_length=10)
     start_date = forms.DateField()
     end_date = forms.DateField()
